e208ee187498deb90c55fea99f69a3951f700c35
Gael Ignacio Sosa Gutiérrez <gaelsosaa@192.168.68.107>
2025-07-29 11:20:13 -0600

    Deploy stable V1.0 with App promotion

--- app.py
+++ app.py
@@ -11,31 +11,23 @@ app = Flask(__name__)
 # Directorio temporal para guardar las descargas
 DOWNLOAD_FOLDER = '/tmp/downloads'
 
-# Asegurarse de que el directorio de descargas exista
 if not os.path.exists(DOWNLOAD_FOLDER):
     os.makedirs(DOWNLOAD_FOLDER)
 
-# Create the main page route
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Ruta para descargar el archivo desde nuestro servidor
 @app.route('/download_file/<filename>')
 def download_file(filename):
     path = os.path.join(DOWNLOAD_FOLDER, filename)
     try:
         if os.path.exists(path):
             return send_file(path, as_attachment=True)
-        else:
-            return "Error: Archivo no encontrado.", 404
     finally:
         if os.path.exists(path):
             os.remove(path)
-            print(f"Archivo temporal {filename} eliminado.")
 
-
-# El endpoint principal de la API
 @app.route('/process', methods=['POST'])
 def process_video():
     data = request.get_json()
@@ -56,32 +48,22 @@ def process_video():
 
         if download_type == 'audio':
             ydl_opts['format'] = 'bestaudio/best'
-            ydl_opts['postprocessors'] = [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }]
+            ydl_opts['postprocessors'] = [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}]
         else: # Video
-            # La instrucción de precisión para videos horizontales y verticales
-            ydl_opts['format'] = 'bestvideo[width=1080][height=1920][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/bestvideo[width<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]'
+            ydl_opts['format'] = 'bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]/best[ext=mp4]'
             ydl_opts['merge_output_format'] = 'mp4'
 
-        print("Descargando video al servidor (versión estable)...")
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=True)
             title = info.get('title', 'Unknown title')
-            
-            # La corrección inteligente para el bug de MP3
             final_filepath = info.get('requested_downloads')[0]['filepath']
             final_filename = os.path.basename(final_filepath)
 
-        print(f"¡ÉXITO! Video descargado al servidor como {final_filename}")
         download_url = f'/download_file/{final_filename}'
         return jsonify({'success': True, 'download_url': download_url, 'title': title})
 
     except Exception as e:
-        print(f"Ocurrió un error: {e}")
-        return jsonify({'success': False, 'error': str(e)}), 500
+        return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
